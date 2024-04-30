38757072357e6a2959c96d5e1f765dc042509e2b
MrGoblings <mrgoblings@gmail.com>
2024-04-30 06:12:08 +0000

    add: EDM

---
+++ codbex-account/gen/ui/entities/Account/dialog-filter/controller.js
@@ -0,0 +1,74 @@
+angular.module('page', ["ideUI", "ideView"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-account.entities.Account';
+	}])
+	.controller('PageController', ['$scope', 'messageHub', 'ViewParameters', function ($scope, messageHub, ViewParameters) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.entity = params.entity ?? {};
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+		}
+
+		$scope.filter = function () {
+			let entity = $scope.entity;
+			const filter = {
+				$filter: {
+					equals: {
+					},
+					notEquals: {
+					},
+					contains: {
+					},
+					greaterThan: {
+					},
+					greaterThanOrEqual: {
+					},
+					lessThan: {
+					},
+					lessThanOrEqual: {
+					}
+				},
+			};
+			if (entity.Id) {
+				filter.$filter.equals.Id = entity.Id;
+			}
+			if (entity.Name) {
+				filter.$filter.contains.Name = entity.Name;
+			}
+			if (entity.Code) {
+				filter.$filter.equals.Code = entity.Code;
+			}
+			if (entity.Active) {
+				filter.$filter.equals.Active = entity.Active;
+			}
+			if (entity.Normal) {
+				filter.$filter.equals.Normal = entity.Normal;
+			}
+			messageHub.postMessage("entitySearch", {
+				entity: entity,
+				filter: filter
+			});
+			$scope.cancel();
+		};
+
+		$scope.resetFilter = function () {
+			$scope.entity = {};
+			$scope.filter();
+		};
+
+		$scope.cancel = function () {
+			messageHub.closeDialogWindow("Account-filter");
+		};
+
+		$scope.clearErrorMessage = function () {
+			$scope.errorMessage = null;
+		};
+
+	}]);
