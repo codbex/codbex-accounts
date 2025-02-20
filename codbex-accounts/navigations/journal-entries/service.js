28b5ae721587e3383da9b031e22fb10f892258d7
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-02-20 19:11:20 +0200

    change journal entry navigation

--- codbex-accounts/navigations/journal-entries/service.js
+++ codbex-accounts/navigations/journal-entries/service.js
@@ -1,7 +1,7 @@
 const navigationData = {
     id: 'journal-entry-navigation',
     label: "Journal Entry",
-    group: "accounts",
+    group: "accounting",
     order: 100,
     link: "/services/web/codbex-accounts/gen/codbex-accounts/ui/JournalEntry/index.html?embedded"
 };
