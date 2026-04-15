736e8c8484acd9a81b18b2ec17035397536dcf26
Tomislav Ivanov <78173711+TIVMOF@users.noreply.github.com>
2026-04-15 10:45:25 +0300

    feat: Added field direction and entity made setting (#23)

--- codbex-accounts/gen/codbex-accounts/api/Settings/JournalEntryDirectionController.ts
+++ codbex-accounts/gen/codbex-accounts/api/Settings/JournalEntryDirectionController.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Documentation, request, response } from '@aerokit/sdk/http'
 import { HttpUtils } from "@aerokit/sdk/http/utils";
 import { ValidationError } from '@aerokit/sdk/http/errors'
-import { ForbiddenError } from '@aerokit/sdk/http/errors'
-import { user } from '@aerokit/sdk/security'
 import { Options } from '@aerokit/sdk/db'
 import { Extensions } from "@aerokit/sdk/extensions"
 import { Injected, Inject } from '@aerokit/sdk/component'
@@ -23,7 +21,6 @@ class JournalEntryDirectionController {
     @Documentation('Get All JournalEntryDirection')
     public getAll(_: any, ctx: any): JournalEntryDirectionEntity[] {
         try {
-            this.checkPermissions('read');
             const options: Options = {
                 limit: ctx.queryParameters["$limit"] ? parseInt(ctx.queryParameters["$limit"]) : 20,
                 offset: ctx.queryParameters["$offset"] ? parseInt(ctx.queryParameters["$offset"]) : 0,
@@ -41,7 +38,6 @@ class JournalEntryDirectionController {
     @Documentation('Create JournalEntryDirection')
     public create(entity: JournalEntryDirectionEntity): JournalEntryDirectionEntity {
         try {
-            this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
             response.setHeader('Content-Location', '/services/ts/codbex-accounts/gen/codbex-accounts/api/Settings/JournalEntryDirectionController.ts/' + entity.Id);
@@ -57,7 +53,6 @@ class JournalEntryDirectionController {
     @Documentation('Count JournalEntryDirection')
     public count(): { count: number } {
         try {
-            this.checkPermissions('read');
             return { count: this.repository.count() };
         } catch (error: any) {
             this.handleError(error);
@@ -69,7 +64,6 @@ class JournalEntryDirectionController {
     @Documentation('Count JournalEntryDirection with filter')
     public countWithFilter(filter: any): { count: number } {
         try {
-            this.checkPermissions('read');
             return { count: this.repository.count(filter) };
         } catch (error: any) {
             this.handleError(error);
@@ -81,7 +75,6 @@ class JournalEntryDirectionController {
     @Documentation('Search JournalEntryDirection')
     public search(filter: any): JournalEntryDirectionEntity[] {
         try {
-            this.checkPermissions('read');
             return this.repository.findAll(filter);
         } catch (error: any) {
             this.handleError(error);
@@ -93,7 +86,6 @@ class JournalEntryDirectionController {
     @Documentation('Get JournalEntryDirection by id')
     public getById(_: any, ctx: any): JournalEntryDirectionEntity {
         try {
-            this.checkPermissions('read');
             const id = parseInt(ctx.pathParameters.id);
             const options: Options = {
                 language: request.getLocale().split("_")[0]
@@ -114,7 +106,6 @@ class JournalEntryDirectionController {
     @Documentation('Update JournalEntryDirection by id')
     public update(entity: JournalEntryDirectionEntity, ctx: any): JournalEntryDirectionEntity {
         try {
-            this.checkPermissions('write');
             const id = parseInt(ctx.pathParameters.id);
             entity.Id = id;
             this.validateEntity(entity);
@@ -130,7 +121,6 @@ class JournalEntryDirectionController {
     @Documentation('Delete JournalEntryDirection by id')
     public deleteById(_: any, ctx: any): void {
         try {
-            this.checkPermissions('write');
             const id = parseInt(ctx.pathParameters.id);
             const entity = this.repository.findById(id);
             if (entity) {
@@ -154,21 +144,12 @@ class JournalEntryDirectionController {
         }
     }
 
-    private checkPermissions(operationType: string) {
-        if (operationType === 'read' && !(user.isInRole('codbex-accounts.Settings.JournalEntryDirectionReadOnly') || user.isInRole('codbex-accounts.Settings.JournalEntryDirectionFullAccess'))) {
-            throw new ForbiddenError();
-        }
-        if (operationType === 'write' && !user.isInRole('codbex-accounts.Settings.JournalEntryDirectionFullAccess')) {
-            throw new ForbiddenError();
-        }
-    }
-
     private validateEntity(entity: any): void {
         if (entity.Name === null || entity.Name === undefined) {
             throw new ValidationError(`The 'Name' property is required, provide a valid value`);
         }
-        if (entity.Name?.length > 200) {
-            throw new ValidationError(`The 'Name' exceeds the maximum length of [200] characters`);
+        if (entity.Name?.length > 20) {
+            throw new ValidationError(`The 'Name' exceeds the maximum length of [20] characters`);
         }
         for (const next of validationModules) {
             next.validate(entity);
